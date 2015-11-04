cb78b777a98fc61e9695c6b18a559b819818256d
anthony_salutari <anthony17183@gmail.com>
2015-11-04 13:46:07 +0000

    account activation completed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'https://sample-app-anthony-salutari.c9.io/' }
 end
